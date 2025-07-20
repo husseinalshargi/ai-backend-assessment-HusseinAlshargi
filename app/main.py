e0505ee2230c6848465b2d051f60879bb8327749
Hussein Adel <x.sky.x@hotmail.com>
2025-07-20 21:54:41 +0300

    added CronTrigger to do the ingestion and eval in certain times

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 # a library that run cli commands which run tasks from the terminal without needing the user to write all the code 
 import typer
 from fastapi import FastAPI
+from contextlib import asynccontextmanager #to handle both start up and shutdown
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 from app.api.report.generate import generate_router
 from app.schemas.create_db_tables import create_tables
@@ -8,18 +11,36 @@ from app.services.ingestion import ingest_files
 from app.services.evaluation import evaluate_answer
 
 typer_app = typer.Typer()
-api_app = FastAPI()
+scheduler = AsyncIOScheduler()
+
 
 #include report generator router to the FastAPI app
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #start up by Adding scheduled jobs and start the scheduler
+    scheduler.add_job(ingest_files, CronTrigger(hour=2, minute=0), id="nightly_refresh") #on 24-hours not 12-hours based
+    scheduler.add_job(evaluate_answer, CronTrigger(hour=3, minute=0), id="nightly_evaluation")
+    scheduler.start()
+    print("Scheduler started.")
+
+    yield  #run the app
+
+    #shutdown
+    scheduler.shutdown()
+    print("Scheduler shut down.")
+
+
+api_app = FastAPI(lifespan=lifespan) #added the function to the api obj so that it starts and shuted down with it
 api_app.include_router(generate_router, prefix="/api/report")
+
+#CLI commands:
 
 #in order to run it in the cli like "python -m app.main nightly-refresh" if the bool is true then add "--now" Note: we are using - not _, also we used main instead of main.py in order to be able to import a file from the same directory whithout issues
 @typer_app.command()
 def nightly_refresh(now : bool = False):
     if (now) : 
         ingest_files()
-    else: 
-        print('Scheduled Refresh')
 
 @typer_app.command()
 def create_tables_in_db():
